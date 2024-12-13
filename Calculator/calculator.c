38f6c62bf8a41b91fb487fb8d37ad3e86c1dadc2
Akshansh <soniakshansh53@gmail.com>
2024-12-13 14:05:47 +0530

    made necessary changes as asked in PR comments

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -1,52 +1,84 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-#define MaxSize 100
-
-int prec(char c) {
-    if(c == '/' || c == '*') {
+int precedence(char operator)
+{
+    if (operator== '/' || operator== '*')
+    {
         return 2;
-    }else if(c == '+' || c == '-') {
+    }
+    else if (operator== '+' || operator== '-')
+    {
         return 1;
-    }else {
+    }
+    else
+    {
         return 0;
     }
 }
 
-int evaluate(int num1, int num2, char op) {
-    if(op == '/') {
-        if(num2 == 0) {
-        printf("Error: Division by zero");
-        exit(0);
+int evaluate(int num1, int num2, char op)
+{
+    if (op == '/')
+    {
+        if (num2 == 0)
+        {
+            printf("Error: Division by zero");
+            exit(0);
         }
-        return num1/num2;
-    }else if(op == '*') {
-        return num1*num2;
-    }else if(op == '+') {
-        return num1+num2;
-    }else if(op == '-') {
-        return num1-num2;
-    }else {
+        return num1 / num2;
+    }
+    else if (op == '*')
+    {
+        return num1 * num2;
+    }
+    else if (op == '+')
+    {
+        return num1 + num2;
+    }
+    else if (op == '-')
+    {
+        return num1 - num2;
+    }
+    else
+    {
         return -1;
     }
 }
 
-int calculator(char *str) {
+void calculateTop(int nums[], char ops[], int *top1, int *top2)
+{
+    int val2 = nums[*top1];
+    (*top1)--;
+    int val1 = nums[*top1];
+    (*top1)--;
+    char op = ops[*top2];
+    (*top2)--;
+
+    (*top1)++;
+    nums[*top1] = evaluate(val1, val2, op);
+}
+
+int calculator(char *str)
+{
     int n = strlen(str) - 1;
     int nums[n];
-    char ops[n];
-    int top1=-1,top2=-1;
+    char operator[n];
+    int top1 = -1, top2 = -1;
 
     for (int i = 0; i < n; i++)
     {
-        if(str[i] == ' ') {
+        if (str[i] == ' ')
+        {
             continue;
-        }else if(isdigit(str[i])) {
+        }
+        else if (str[i] >= '0' && str[i] <= '9')
+        {
             int val = 0;
 
-            while (i < n && isdigit(str[i]))
+            while (i < n && (str[i] >= '0' && str[i] <= '9'))
             {
                 val = (val * 10) + (str[i] - '0');
                 i++;
@@ -54,46 +86,39 @@ int calculator(char *str) {
             top1++;
             nums[top1] = val;
             i--;
-        }else if(str[i] == '/' || str[i] == '*' || str[i] == '+' || str[i] == '-') {
-            while (top2 != -1 && prec(str[i]) <= prec(ops[top2]))
+        }
+        else if (str[i] == '/' || str[i] == '*' || str[i] == '+' || str[i] == '-')
+        {
+            while (top2 != -1 && precedence(str[i]) <= precedence(operator[top2]))
             {
-                int val2 = nums[top1];
-                top1--;
-                int val1 = nums[top1];
-                top1--;
-                char op = ops[top2];
-                top2--;
-
-                top1++;
-                nums[top1] = evaluate(val1,val2,op);
+                calculateTop(nums, operator, &top1, &top2);
             }
             top2++;
-            ops[top2] = str[i];
-        }else{
+            operator[top2] = str[i];
+        }
+        else
+        {
             printf("Invalid Expression");
             return 0;
-            break;
         }
     }
     while (top2 != -1)
     {
-        int val2 = nums[top1];
-        top1--;
-        int val1 = nums[top1];
-        top1--;
-
-        char op = ops[top2];
-        top2--;
-
-        top1++;
-        nums[top1] = evaluate(val1,val2,op);
+        calculateTop(nums, operator, &top1, &top2);
     }
     return nums[top1];
 }
 
-int main() {
-    char str[MaxSize];
-    fgets(str, MaxSize, stdin);
+int main()
+{
+    int size;
+    printf("Enter the size of String ");
+    scanf("%d", &size);
+    getchar();
 
-    printf("%d",calculator(str));
+    char str[size];
+    fgets(str, size, stdin);
+
+    printf("%d", calculator(str));
+    return 0;
 }
