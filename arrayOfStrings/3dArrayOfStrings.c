48f5437840f8a615d77c36e099265b46d6751b84
akshansh711 <97897404+akshansh711@users.noreply.github.com>
2025-01-08 17:24:50 +0530

    Implemented valid integer edgecase

--- arrayOfStrings/3dArrayOfStrings.c
+++ arrayOfStrings/3dArrayOfStrings.c
@@ -1,12 +1,47 @@
 #include<stdio.h>
 #include<string.h>
 
+int findLength(char str[]) {
+    int len = 0;
+    while(str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+void stringCopy(char to[], char from[]) {
+    int i=0;
+    
+    while(from[i] != '\0') {
+        to[i] = from[i];
+        i++;
+    }
+    to[i] = '\0';
+}
+
+int validInput(int min) {
+    int value;
+    while(scanf("%d",&value) != 1 || value < min) {
+        printf("Invalid input !!!");
+        return -1;
+    }
+    return value;
+}
+
 int main() {
     int rows,columns;
     printf("Enter number of rows ");
-    scanf("%d",&rows);
+    rows = validInput(0);
+    
+    if(rows == -1) {
+        return 0;
+    }
     printf("Enter number of columns ");
-    scanf("%d",&columns);
+    columns = validInput(0);
+    
+    if(columns == -1) {
+        return 0;
+    }
 
     char students[rows][columns][50];
 
@@ -31,35 +66,37 @@ int main() {
     int cnt = 0;
     for (int i = 0; i < rows; i++)
     {
-        for(int j = 0; j < columns; i++)
+        for(int j = 0; j < columns; j++)
         {
             if(
-              students[i][j][0] == 'a' ||students[i][j][0] == 'e' ||
-               students[i][j][0] == 'i' ||students[i][j][0] == 'o' ||
-               students[i][j][0] == 'u' ||students[i][j][0] == 'A' ||
-               students[i][j][0] == 'E' ||students[i][j][0] == 'I' || 
-               students[i][j][0] == 'O' ||students[i][j][0] == 'U' 
+               students[i][j][0] == 'a' || students[i][j][0] == 'e' ||
+               students[i][j][0] == 'i' || students[i][j][0] == 'o' ||
+               students[i][j][0] == 'u' || students[i][j][0] == 'A' ||
+               students[i][j][0] == 'E' || students[i][j][0] == 'I' || 
+               students[i][j][0] == 'O' || students[i][j][0] == 'U' 
             ){
                 cnt++;
             }
         }
     }
     
+    printf("Number of elements starting with a vowel: %d\n",cnt);
+    
     int maxLength = 0;
     char longestName[50];
 
     for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < columns; i++)
+        for (int j = 0; j < columns; j++)
         {
-            int len = strlen(students[i][j]);
+            int len = findLength(students[i][j]);
             if(len > maxLength) {
                 maxLength = len;
-                strcpy(longestName, students[i][j]);
+                stringCopy(longestName, students[i][j]);
             }
         }
         
     }
     
-    printf("The longest name is ",longestName);
+    printf("The longest name is %s",longestName);
 }
