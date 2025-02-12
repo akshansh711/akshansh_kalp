e7fdb415356442880bff427e77d060fc2ab7c6f1
akshansh711 <97897404+akshansh711@users.noreply.github.com>
2025-02-12 14:30:05 +0530

    Updated first non repeating character (previous code was not working for all testcases).

--- firstNonRepeatingChar.c
+++ firstNonRepeatingChar.c
@@ -2,28 +2,36 @@
 #include<stdlib.h>
 
 int main() {
-    int freq[26] = {0};
+    int frequency[26] = {0};
 
-    char str[100];
-    scanf("%s",str);
+    char input[100];
+    scanf("%s",input);
 
     int i=0;
     char fnr = '0';
-    int flag = 0;
-    while(str[i] != '\0') {
-        if (freq[str[i] - 'a'] == 0)
+    
+
+    while (input[i] != '\0')
+    {
+        frequency[input[i] - 'a']++;
+
+        int fnd = 0;
+        for (int j = 0; j <= i ; j++)
         {
-            if(fnr == '0') {
-                printf("%c",str[i]);
-                fnr = str[i];
-            }else{
-                printf("%c",fnr);
+            if (frequency[input[j] - 'a'] == 1)
+            {
+                printf("%c",input[j]);
+                fnd = 1;
+                break;
             }
-            freq[str[i] - 'a']++;
-        }else if(freq[str[i] - 'a'] > 0) {
+        }
+        if (!fnd)
+        {
             printf("-1");
-            fnr = '0';
         }
         i++;
+        
     }
+    
+
 }
